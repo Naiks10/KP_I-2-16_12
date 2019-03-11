a30e38b441da7c9675e9e3c23e7dfeafe53344b9
naiks10 <Ultravirus661@gmail.com>
2019-03-11 22:27:27 +0300

    main

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
     Connection mainConnection;
     mainConnection.connect(
                 "localhost",
-                "dbase",
+                "wbase",
                 "postgres",
                 "09102000"
              );
